c949623daa9da0d2b2e9f6ae1f9f8548254513c5
이재현 <ijaehyeon@ijaehyeons-MacBook-Air.local>
2023-08-15 07:02:18 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# miracle_study
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/miracle_study/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.miracle_study
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
